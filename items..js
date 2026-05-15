cfd2b66c6e44230c7cab6975b2b781c2a77bdf88
weh385 <weholland@morrisbb.net>
2026-05-15 09:46:05 -0400

    Add files via upload

---
+++ items..js
@@ -0,0 +1,50 @@
+const ITEMS = [
+  {
+    category: "hand-tools",
+    image: "stanley-plane.jpg",
+    name: "Stanley #4 Smoothing Plane",
+    description: "Good condition. No rust. Sharp.",
+    price: "50.00",
+    status: "available"
+  },
+  {
+    category: "power-tools",
+    image: "table-saw.jpg",
+    name: "Example Power Tool",
+    description: "Replace this with the item description.",
+    price: "100.00",
+    status: "sold"
+  },
+  {
+    category: "wood-supplies",
+    image: "wood-supplies-1.jpg",
+    name: "Example Wood or Supply Item",
+    description: "Replace this with the item description.",
+    price: "25.00",
+    status: "available"
+  },
+  {
+    category: "art",
+    image: "art-1.jpg",
+    name: "Example Art Item",
+    description: "Replace this with the item description.",
+    price: "75.00",
+    status: "available"
+  },
+  {
+    category: "household",
+    image: "household-1.jpg",
+    name: "Example Household Item",
+    description: "Replace this with the item description.",
+    price: "40.00",
+    status: "available"
+  },
+  {
+    category: "other",
+    image: "other-1.jpg",
+    name: "Example Other Item",
+    description: "Replace this with the item description.",
+    price: "20.00",
+    status: "available"
+  }
+];
