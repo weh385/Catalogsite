8d4ab7fe346c6f17d5b8944b6ab4e1959369c870
weh385 <weholland@morrisbb.net>
2026-05-17 15:41:25 -0400

    Update items.js with new catalog items

---
+++ items.js
@@ -0,0 +1,82 @@
+const ITEMS = [
+  {
+    category: "hand-tools",
+    image: "stanley-plane.jpg",
+    name: "Stanley #4 Smoothing Plane",
+    description: "Good condition. Rusty. Not Sharp.",
+    price: "50.00",
+    status: "available"
+  },
+  {
+    category: "power-tools",
+    image: "table-saw.jpg",
+    name: "Example Power Tool",
+    description: "Old Craftsman  10 in blade.",
+    price: "100.00",
+    status: "available"
+  },
+  {
+    category: "wood-supplies",
+    image: "wood-supplies-1.jpg",
+    name: "Example Wood or Supply Item",
+    description: "Replace this with the item description.",
+    price: "25.00",
+    status: "sold"
+  },
+  {
+    category: "art",
+    image: "art-1.jpg",
+    name: "Example Art Item",
+    description: "Replace this with the item description.",
+    price: "75.00",
+    status: "available"
+  },
+  {
+    category: "household",
+    image: "household-1.jpg",
+    name: "Example Household Item",
+    description: "Replace this with the item description.",
+    price: "40.00",
+    status: "sold"
+  },
+  {
+    category: "hand-tools",
+    image: "Dog1.jpg",
+    name: "Cocker Spaniel",
+    description: "Good dog.  Well trained.",
+    price: "50.00",
+    status: "available"
+  },
+  {
+    category: "other",
+    image: "other-1.jpg",
+    name: "Example Other Item",
+    description: "Replace this with the item description.",
+    price: "20.00",
+    status: "available"
+  },
+  {
+    category: "art",
+    image: "stanley-plane.jpg",
+    name: "Stanley #4 Smoothing Plane",
+    description: "Good condition. No Rust Dull as hell.",
+    price: "50.00",
+    status: "available"
+  },
+  {
+    category: "hand-tools",
+    image: "dog2.jpg",
+    name: "Clumber",
+    description: "Slow, lazy, best dog I ever had",
+    price: "5000.00",
+    status: "available"
+  },
+  {
+    category: "art",
+    image: "stanley-plane.jpg",
+    name: "Stanley #4 Smoothing Plane",
+    description: "Good condition. Shar[].",
+    price: "50.00",
+    status: "available"
+  }
+];
